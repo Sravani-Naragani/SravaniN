7215a71eeea5dcd3bbe1de2f4fc58b19c6729e26
Sravani-Naragani <109466361+Sravani-Naragani@users.noreply.github.com>
2022-11-09 23:57:28 +0530

    Add files via upload

---
+++ LmsWeb/LmsWebHib/src/main/java/com/infinite/lmswebhib/LeaveHistoryDAO.java
@@ -0,0 +1,20 @@
+package com.infinite.lmswebhib;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class LeaveHistoryDAO {
+    SessionFactory sessionFactory;
+    public List<LeaveHistory> showLeaveHistory(int empId) {
+        sessionFactory = SessionHelper.getSession();
+ Session session=sessionFactory.openSession(); 
+ Query query = session.getNamedQuery("searchLeaveHistory");
+ query.setParameter("empId", empId);
+ List<LeaveHistory> leavehistory = query.list();
+ return leavehistory;
+
+}
+}
---
+++ LmsWeb/LmsWebHib/src/main/java/com/infinite/lmswebhib/LeaveType.java
@@ -0,0 +1,5 @@
+package com.infinite.lmswebhib;
+
+public enum LeaveType {
+EL,PL,ML
+}
